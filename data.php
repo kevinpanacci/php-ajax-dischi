bd2b1f870dd43384e9d84e44889923cfb34ef9e1
Kevin <kevinpanacci@hotmail.it>
2020-04-22 09:28:26 +0200

    Duplicate cards

--- data.php
+++ data.php
@@ -19,6 +19,42 @@
             'anno' => '2001',
             'immagine' => 'https://images-na.ssl-images-amazon.com/images/I/91NQO2l3RZL._AC_SL1500_.jpg'
         ],
+        [
+            'titolo' => 'Numb',
+            'autore' => 'Linkin Park',
+            'anno' => '2003',
+            'immagine' => 'https://img.redbull.com/images/c_fill,g_auto,w_860,h_1075/q_auto,f_auto/redbullcom/2018/03/23/760ca82f-642d-4d4c-806a-fd6b511172f4/meteora'
+        ],
+        [
+            'titolo' => 'Leave out all the rest',
+            'autore' => 'Linkin Park',
+            'anno' => '2008',
+            'immagine' => 'https://m.media-amazon.com/images/I/71jI8WPXmSL._SS500_.jpg'
+        ],
+        [
+            'titolo' => 'Crawling',
+            'autore' => 'Linkin Park',
+            'anno' => '2001',
+            'immagine' => 'https://images-na.ssl-images-amazon.com/images/I/91NQO2l3RZL._AC_SL1500_.jpg'
+        ],
+        [
+            'titolo' => 'Numb',
+            'autore' => 'Linkin Park',
+            'anno' => '2003',
+            'immagine' => 'https://img.redbull.com/images/c_fill,g_auto,w_860,h_1075/q_auto,f_auto/redbullcom/2018/03/23/760ca82f-642d-4d4c-806a-fd6b511172f4/meteora'
+        ],
+        [
+            'titolo' => 'Leave out all the rest',
+            'autore' => 'Linkin Park',
+            'anno' => '2008',
+            'immagine' => 'https://m.media-amazon.com/images/I/71jI8WPXmSL._SS500_.jpg'
+        ],
+        [
+            'titolo' => 'Crawling',
+            'autore' => 'Linkin Park',
+            'anno' => '2001',
+            'immagine' => 'https://images-na.ssl-images-amazon.com/images/I/91NQO2l3RZL._AC_SL1500_.jpg'
+        ],
     ];
 
 ?>
